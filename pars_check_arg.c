834feeb0b02a05b24175734167be45bd0007e7d6
aroualid <aroualid@student.42.fr>
2024-02-23 18:02:01 +0100

    pars nickel

--- pars_check_arg.c
+++ pars_check_arg.c
@@ -6,7 +6,7 @@
 /*   By: aroualid <aroualid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 13:59:49 by aroualid          #+#    #+#             */
-/*   Updated: 2024/02/23 16:21:27 by aroualid         ###   ########.fr       */
+/*   Updated: 2024/02/23 17:49:26 by aroualid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,13 @@ int	check_if_no_digit(char **str)
 		j = 0;
 		while (str[i][j])
 		{
-			if (ft_isdigit(str[i][j]) == 1)
+			if (ft_isdigit(str[i][j]) == 1 || (str[i][j] == '-'
+				|| str[i][j] == '+'))
 			{
+				if ((str[i][j] == '-' || str[i][j] == '+')
+					&& (ft_isdigit(str[i][j - 1]) == 1
+					|| ft_isdigit(str[i][j + 1]) == 0))
+					return (0);
 				j++;
 			}
 			else
@@ -35,44 +40,23 @@ int	check_if_no_digit(char **str)
 	return (1);
 }
 
-int *check_good_arg(char **str, int ac, char *av[])
+int	*check_good_arg(char **str, int ac, char *av[])
 {
-	int	i;
-	int *dtr;
-	char **test;
+	int		i;
+	int		*dtr;
+	char	**test;
 
 	dtr = malloc (sizeof (char *) * (size_to_malloc(ac, av) + 1));
 	i = 0;
 	if (check_if_no_digit(str) == 0)
-		return NULL;
+		return (NULL);
 	else
 	{
-		while(str[i] != NULL)
+		while (str[i] != NULL)
 		{
 			dtr[i] = ft_atoi(str[i]);
 			i++;
 		}
 		return (dtr);
-	}
-}
-
-int    main(int ac, char **av)
-{
-	char **str = NULL;
-  int *dtr = NULL;
-  int i = 0;
-	int	j = 0;
-
-  str = check_arg(ac, av, &j);
-  dtr = check_good_arg(str, ac, av);
-	if (dtr == NULL)
-		return (1);
-	else
-	{
-		while (i < j) 
-		{
-			printf("%d\n", dtr[i]);
-			i++;
-		}
 	}
 }
