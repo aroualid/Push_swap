6856d7438cfce8117811692898e87910f73f0738
aroualid <ari.oualid@gmail.com>
2024-03-10 03:20:51 +0100

    OMG

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: aroualid <aroualid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 13:32:12 by aroualid          #+#    #+#             */
-/*   Updated: 2024/03/10 03:06:52 by aroualid         ###   ########.fr       */
+/*   Updated: 2024/03/10 03:20:15 by aroualid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,19 +28,19 @@ int main(int ac, char **av)
 	b->data = ft_calloc(sizeof(int), j);
 	str = str;
 	i = a->size;
-	/*printf ("#####stack a before sort######\n");
+	printf ("#####stack a before sort######\n");
 	while (k < i)
 	{
 		printf("[%d] ", a->data[k]);
 		k++;
 	}
-	printf("\n");*/
+	printf("\n");
 	push_two(a, b);
 	apply_sort(a,b);
-	/*printf("\n\n\n#####stack a after sort#####\n");
+	printf("\n\n\n#####stack a after sort#####\n");
 	for (int i = 0; i < a->size; i++)
 		printf("[%d] ", a->data[i]);
 	printf("\n#####stack b after sort#####\n");
 	for (int i = 0; i < b->size; i++)
-		printf("[%d] ", b->data[i]);*/
+		printf("[%d] ", b->data[i]);
 }
